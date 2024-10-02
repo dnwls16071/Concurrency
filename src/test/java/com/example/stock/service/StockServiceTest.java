e06a07edb47b44a013d4bd856d1bb283e9b9c507
장우진 <dnwls16071@naver.com>
2024-10-02 13:18:15 +0900

    :test_tube:  멀티 쓰레드 환경에서의 테스트 코드 추가

--- src/test/java/com/example/stock/service/StockServiceTest.java
+++ src/test/java/com/example/stock/service/StockServiceTest.java
@@ -10,6 +10,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 @SpringBootTest
 class StockServiceTest {
 
@@ -32,5 +36,26 @@ class StockServiceTest {
 		stockService.decrease(1L, 1L);
 		Stock stock = stockRepository.findById(1L).orElseThrow();
 		Assertions.assertThat(stock.getQuantity()).isEqualTo(99);
+	}
+	
+	@Test
+	@DisplayName("동시에 100개의 요청이 들어올 경우 테스트 코드")
+	void decrease_concurrency() throws InterruptedException {
+		int threadCount = 100;
+		ExecutorService executorService = Executors.newFixedThreadPool(32);
+		CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+
+		for (int i = 0; i < threadCount; i++) {
+			executorService.submit(() -> {
+				try {
+					stockService.decrease(1L, 1L);
+				} finally {
+					countDownLatch.countDown();
+				}
+			});
+		}
+		countDownLatch.await();
+		Stock stock = stockRepository.findById(1L).orElseThrow();
+		Assertions.assertThat(stock.getQuantity()).isEqualTo(0);
 	}
 }
